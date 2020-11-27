f22358db966a7c285a76ce2d47289dbadf91c2aa
**** <2993435325@qq.com>
2020-11-27 22:25:29 +0800

    user,createUser

---
+++ library/src/components/second/create.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="box">
+      <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="图书编号">
+        <el-input v-model="form.id" style="width: 40%;"></el-input>
+      </el-form-item>
+      <el-form-item label="书名">
+        <el-input v-model="form.bookname" style="width: 40%;"></el-input>
+      </el-form-item>
+      <el-form-item label="作者">
+        <el-input v-model="form.auther" style="width: 40%;"></el-input>
+      </el-form-item>
+      <el-form-item label="类型">
+        <el-input v-model="form.type" style="width: 40%;"></el-input>
+      </el-form-item>
+      <el-form-item label="出版商">
+        <el-input v-model="form.publisher" style="width: 40%;"></el-input>
+      </el-form-item>
+      <el-form-item label="库存">
+        <el-input v-model="form.stock" style="width: 40%;"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button @click="getBack">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+      fileList: "",
+      form: {
+        bookname: "夏洛的网",
+        auther: "EB怀特",
+        type: "文学作品",
+        publisher: "凤凰出版社",
+        stock: "4812",
+        id: "FB154"
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$http({
+        url: "/api/book/add_book",
+        method: 'post',
+        params: {
+          bookname: this.form.bookname,
+          author: this.form.auther,
+          id: this.form.id,
+          booktype: this.form.type,
+          publisher: this.form.publisher,
+          stock: this.form.stock 
+        }
+      })
+        .then(res => {
+          console.log(res, "res");
+          // if (res.data.code == 0) {
+          //   this.$message({
+          //     message: res.data.info,
+          //     type: "success"
+          //   });
+            this.$router.push("/index");
+          // } else {
+          //   this.$message({
+          //     message: res.data.info,
+          //     type: "error"
+          //   });
+          // }
+        })
+        .catch(err => {});
+    },
+    //返回上一层
+    getBack() {
+      this.$router.go(-1);
+    }
+  }
+};
+</script>
+<style lang="" scoped>
+#box {
+  flex: 1;
+}
+</style>
---
+++ library/src/components/second/index.vue
@@ -0,0 +1,199 @@
+<template>
+  <div>
+    <div class="top">
+      <el-button type="primary" @click="create">新建</el-button>
+      <div>
+        <input type="text" v-model="value" @input="dimFind(value)" />
+        <el-button type="primary" @click="dimFind">查询</el-button>
+      </div>
+    </div>
+    <div class="block">
+      <el-table :data="messageList">
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="id" v-model="id" label="图书编号"></el-table-column>
+        <el-table-column prop="bookname" v-model="bookname" label="书名"></el-table-column>
+        <el-table-column prop="author" v-model="author" label="作者"></el-table-column>
+        <el-table-column prop="type" v-model="type" label="类型"></el-table-column>
+        <el-table-column prop="publisher" v-model="publisher" label="出版商"></el-table-column>
+        <el-table-column prop="stock" v-model="stock" label="库存"></el-table-column>
+        <el-table-column prop label="操作" width="200px">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-size="5"
+        layout="total, prev, pager, next, jumper"
+        :total="count"
+      ></el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      messageList: [ ],
+      currentPage4: 1,
+      fileList: "",
+      value: "",
+      page: 5,
+      count: 0,
+
+      id: '1',
+      bookname: '',
+      author:'',
+      type: '',
+      publisher: '',
+      stock:''     
+    };
+  },
+  mounted() {
+    //获取
+    this.$http({
+      url: "/api/book/getlist"
+    })
+      .then(res => {
+        console.log(res);
+        this.messageList = [];
+        this.bookname = res.data.bookname,
+        this.author = messageList.author,
+        this.type = messageList.type,
+        this.publisher = messageList.publisher,
+        this.tock = messageList.stock,
+        console.log("success");
+      })
+      .catch(err => {
+        console.log("error");
+      });
+
+  },
+  methods: {
+    //模糊查询
+    dimFind() {
+      this.$http({
+        url: "/api/book/find_id"
+      })
+        .then(res => {
+          // console.log(res, "查询图书成功");
+          // this.messageList = [];
+          // res.data.data.map((item, index) => {
+          //   if (item.name.indexOf(this.value) != -1||item.writer.indexOf(this.value) != -1) {
+          //     this.messageList.push(item);
+          //   }
+          // });
+          // this.value = "";
+        })
+        .catch(err => {
+          // console.log(err, "查询图书失败");
+        });
+    },
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row);
+        });
+      } else {
+        this.$refs.multipleTable.clearSelection();
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    //根据页数判断数据
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`);
+      // this.$http({
+      //   url: "/api/findDevice"
+      // })
+      //   .then(res => {
+      //     // console.log(res, "查询图书成功2222");
+      //     this.messageList = [];
+      //     res.data.data.map((item, index) => {
+      //       if (index >= 0 + 5 * (val - 1) && index < 5 + 5 * (val - 1)) {
+      //         //得到数据库里的数据
+      //         this.messageList.push(item);
+      //       }
+      //     });
+      //     //总条数
+      //     this.count = res.data.data.length;
+      //   })
+      //   .catch(err => {
+      //     console.log(err, "查询图书失败2222");
+      //   });
+    },
+    //跳转到创建页面，实现创建功能
+    create() {
+      this.$router.push("/create");
+      sessionStorage.setItem("isShow", false);
+    },
+    //跳转到编辑页面，实现编辑功能
+    handleEdit(index, row) {
+      console.log(index, row);
+      this.fileList = JSON.stringify(row);
+      console.log(this.fileList, "111");
+      this.$router.push("/create");
+      sessionStorage.setItem("isShow", true);
+      sessionStorage.setItem("filelist", this.fileList);
+    },
+    //删除
+    handleDelete(index, row) {
+      this.$http({
+        url: "/api/book/delbook",
+        params: {
+          number: this.messageList[index].number
+        }
+      })
+        .then(res => {
+          console.log(res, "删除成功");
+          this.find();
+        })
+        .catch(err => {
+          console.log(err, "删除失败");
+        });
+    },
+    //查找
+    find() {
+      this.$http({
+        url: "/api/book/find_id"
+      })
+        .then(res => {
+          console.log(res, "查询图书成功");
+          // this.messageList = res.data.data;
+          // this.count = this.messageList.length;
+        })
+        .catch(err => {
+          console.log(err, "查询图书失败");
+        });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.el-pagination {
+  margin-top: 30px;
+  text-align: center;
+}
+.top {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+.top input {
+  height: 28px;
+}
+</style>
